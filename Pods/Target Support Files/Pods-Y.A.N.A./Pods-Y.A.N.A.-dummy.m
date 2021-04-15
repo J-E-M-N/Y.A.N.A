1beeea5a490ae942e4c29ed851b001045a3e8808
Nigel Powell <nigel.powell@bison.howard.edu>
2021-04-14 20:53:20 -0400

    Initial Commit

---
+++ Pods/Target Support Files/Pods-Y.A.N.A./Pods-Y.A.N.A.-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Y_A_N_A_ : NSObject
+@end
+@implementation PodsDummy_Pods_Y_A_N_A_
+@end
---
+++ Y.A.N.A.Tests/Y_A_N_A_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Y_A_N_A_Tests.swift
+//  Y.A.N.A.Tests
+//
+//  Created by Nigel Powell on 4/14/21.
+//
+
+import XCTest
+@testable import Y_A_N_A_
+
+class Y_A_N_A_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
